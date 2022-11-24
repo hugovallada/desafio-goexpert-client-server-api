e8a52960bf85fa07ad5b461d442cc1791324a6cd
Hugo Vallada <valladahugo@gmail.com>
2022-11-24 00:01:43 -0300

    Remove o banco e adiciona uma função init para criar a tabela

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -17,6 +17,32 @@ var (
 	INTERNAL_ERROR_MESSAGE = "Erro interno do servidor"
 )
 
+func init() {
+	db, err := sql.Open("sqlite3", "./data/price.db")
+	if err != nil {
+		panic(err)
+	}
+	_, tableCheck := db.Query("select * from prices")
+	if tableCheck != nil {
+		log.Println("Criando a tabela 'prices'")
+		db.Exec(`create table prices(
+			id integer primary key autoincrement,
+			  code text,
+			  codein text,
+			  name text,
+			  high decimal(10,5),
+			  low decimal(10,5),
+			  varBid decimal(10,5),
+			  pctChange decimal(10,5),
+			  bid decimal(10,5),
+			  ask decimal(10,5),
+			  timestamp integer,
+			  create_date text
+		);`)
+	}
+
+}
+
 func main() {
 	http.HandleFunc("/cotacao", GetPrice)
 	http.ListenAndServe(":8080", nil)
